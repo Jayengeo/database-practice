0438242926635687a380266a2aae3bccae781816
Jacky Ngo <jacky.ngo96@gmail.com>
2023-12-13 23:14:06 -0500

    create students table

---
+++ migrations/students_cohorts.sql
@@ -0,0 +1,11 @@
+CREATE TABLE students ( 
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(225) NOT NULL, 
+    email VARCHAR(255),
+    phone VARCHAR(32),
+    github VARCHAR(255),
+    start_date Date, 
+    end_date Date, 
+    cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
+); 
+
